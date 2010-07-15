c1dc8cdfdce2a646a52e9092bc10cd1c60620ad6
Meder Omuraliev <meder@dunross.sabre.com>
2010-07-15 14:55:31 -0400

    edits

--- mson.js
+++ mson.js
@@ -1,8 +1,69 @@
 (function() {
 
-    function mson( data ) {
+    var 
+        // current character in loop
+        char
+
+        // current token ( string, array, object )
+        , token
+
+        // map of types
+        , typesMap = {
+            'object' : '{}',
+            'array'  : '[]',
+            'whitespace' : '\n\s\t',
+            'stringdelimiter' : '\'"'
+        }
+        , log = function( msg ) {
+            if ( window.console && console.log ) {
+                console.log.apply( console, arguments );
+            }
+        }
+        ;
+
+    function parse ( data ) {
+        var
+            // data counter
+            i = 0
+
+            // length of json string
+            , l = data.length
+            ;
+
+        for ( ; i<l; ++i ) {
+            char = data[i];
+
+            temp = findToken( char );
+            log( temp )
+
+            if ( !token ) {
+                if ( temp !== 'whitespace' || temp !== 'object' ) {
+                    throw new SyntaxError( 'Unexpected token' );
+                }
+            }
+        }
     }
 
-    window.mson = mson;
+    function findToken ( c ) {
+        var type;
+        if ( !token ) {
+            for ( var prop in typesMap ) {
+                if ( typesMap.hasOwnProperty( prop ) ) {
+                    log( typesMap[prop] )
+                    if ( typesMap[prop].indexOf( c ) != -1 ) {
+                        type = prop
+                    } else {
+                        type = 'string'
+                    }
+
+                    log ( type )
+                }
+            }
+            return type
+        }
+    }
+
+    window.mson = parse;
+    window.findToken = findToken;
 
 })();
